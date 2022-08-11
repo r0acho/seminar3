b6d56f8975fda5d45681ea4b05f577424701e9dc
sweetie <ivan.linzov@gmail.com>
2022-08-11 21:59:48 +0300

    added task21

--- task17/Program.cs
+++ task17/Program.cs
@@ -2,29 +2,35 @@
 //  вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и 
 //  выдаёт номер четверти плоскости, в которой находится эта точка.
 
-Console.WriteLine("Введите координату по X");
-int X = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату по X");
+// int X = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату по Y");
-int Y = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату по Y");
+// int Y = Convert.ToInt32(Console.ReadLine());
 
-if (X > 0 && Y > 0)
-{
-    Console.WriteLine("Точка находится в первой координатной четверти");
-}
-else if (X < 0 && Y > 0)
-{
-    Console.WriteLine("Точка находится во второй координатной четверти");
-}
-else if (X < 0 && Y < 0)
-{
-    Console.WriteLine("Точка находится в третьей координатной четверти");
-}
-else if(X > 0 && Y < 0)
-{
-    Console.WriteLine("Точка находится в четвертой координатной четверти");
-}
-else
-{
-    Console.WriteLine("Мы не можем определить координатную четверть точки");
-}
+// if (X > 0 && Y > 0)
+// {
+//     Console.WriteLine("Точка находится в первой координатной четверти");
+// }
+// else if (X < 0 && Y > 0)
+// {
+//     Console.WriteLine("Точка находится во второй координатной четверти");
+// }
+// else if (X < 0 && Y < 0)
+// {
+//     Console.WriteLine("Точка находится в третьей координатной четверти");
+// }
+// else if(X > 0 && Y < 0)
+// {
+//     Console.WriteLine("Точка находится в четвертой координатной четверти");
+// }
+// else
+// {
+//     Console.WriteLine("Мы не можем определить координатную четверть точки");
+// }
+
+double sqr = 25;
+double n = Math.Sqrt(sqr + 200);
+
+double res = Math.Pow(n, 3);
+Console.WriteLine(res);
---
+++ task21/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+Console.WriteLine("Введите координату по X первой точки");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату по Y первой точки");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату по X второй точки");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату по Y второй точки");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double res = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+Console.WriteLine($"{res:f2}");
